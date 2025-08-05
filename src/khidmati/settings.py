915368a6a70e076cd55663f6ef2fb695dcf81212
Mokhta-r <mahfoudhmokhtar4@gmail.com>
2025-08-05 15:44:41 +0000

    Update settings.py

--- src/khidmati/settings.py
+++ src/khidmati/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3r(4krwet8%4iz0)s*y5j3wdt_e)i6#iule0orp@e_x1hs5d1t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
