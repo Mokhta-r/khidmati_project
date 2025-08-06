ad8269e0ab9d3024bcd8a00262b8075601c21340
Mokhta-r <mahfoudhmokhtar4@gmail.com>
2025-08-06 14:46:27 +0000

    Update settings.py

--- src/khidmati/settings.py
+++ src/khidmati/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'khidmati.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'Templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
